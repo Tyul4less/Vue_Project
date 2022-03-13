f949ba0f77e20472878fd034c50a8f78afe4f0eb
soeunyoo <scipiomin@gmail.com>
2022-03-13 16:16:26 +0900

    대원이 수정후

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import VueRouter from 'vue-router'
 import baseRoute from './hr/BaseRoute'
 import logiRouter from './logi/BaseRoute'
 import accountRouter from './account/BaseRoute'
+import salesRoute from '@/router/logi/SalesRoute'
 
 Vue.use(VueRouter)
 
@@ -18,6 +19,7 @@ const router = new VueRouter({
     ...baseRoute,
     ...accountRouter,
     ...logiRouter,
+    ...salesRoute,
     {
       path: '/',
       name: 'home',
---
+++ src/views/logistic/sales/Estimate/EstimateInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <estimate-infotable1></estimate-infotable1>
+    <estimate-info-table2></estimate-info-table2>
+  </div>
+</template>
+
+<script>
+// eslint-disable-next-line import/no-absolute-path,import/no-unresolved
+import estimateInfotable1  from '/src/components/logistic/sales/estimate/EstimateInfoTable1.vue';
+import estimateInfoTable2 from '@/components/logistic/sales/estimate/EstimateInfoTable2'
+
+export default {
+  components: {
+    estimateInfotable1,
+    estimateInfoTable2,
+  },
+}
+</script>
