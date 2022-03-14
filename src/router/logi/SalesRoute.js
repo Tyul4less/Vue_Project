1aaca93b3e4201d5f66d08f084372989f771d6fd
Tyul4less <dnjsdl4646@naver.com>
2022-03-14 09:11:15 +0900

    토요일 작업/수주, 납품관리, 메뉴

--- src/router/logi/SalesRoute.js
+++ src/router/logi/SalesRoute.js
@@ -1,3 +1,62 @@
-export default {
+const ERP = '/logi'
+const salesRoute = [
+  {
+    path: `${ERP}/contractInfo`,
+    name: 'contractInfo',
+    component: () => import('@/views/logistic/sales/contract/contractInfo/contractInfo'),
+    meta: {
+      pageTitle: '수주 조회',
+      breadcrumb: [
+        {
+          text: '영업 관리',
+        },
+        {
+          text: '수주 관리',
+        },
+        {
+          text: '수주 조회',
+          active: true,
+        },
+      ],
+    },
+  },
+  {
+    path: `${ERP}/registContract`,
+    name: 'registContract',
+    component: () => import('@/views/logistic/sales/contract/registContract/contractRegist'),
+    meta: {
+      pageTitle: '수주 등록',
+      breadcrumb: [
+        {
+          text: '영업 관리',
+        },
+        {
+          text: '수주 관리',
+        },
+        {
+          text: '수주 등록',
+          active: true,
+        },
+      ],
+    },
+  },
+  {
+    path: `${ERP}/deliveryInfoPage`,
+    name: 'deliveryInfoPage',
+    component: () => import('@/views/logistic/sales/deliveryInfo/deliveryInfoPage'),
+    meta: {
+      pageTitle: '수주 등록',
+      breadcrumb: [
+        {
+          text: '영업 관리',
+        },
+        {
+          text: '납품 관리',
+          active: true,
+        },
+      ],
+    },
+  },
+]
 
-}
+export default salesRoute
